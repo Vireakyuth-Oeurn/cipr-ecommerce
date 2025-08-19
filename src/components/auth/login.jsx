595f9f0851e8928555185dd668b3c1a6ddadc2f5
phanreach <reach4phan@gmail.com>
2025-08-19 23:01:42 +0700

    fix end point for login

--- src/components/auth/login.jsx
+++ src/components/auth/login.jsx
@@ -22,7 +22,7 @@ function LoginPage() {
         setErrors({});
         
         try {
-            const res = await fetch(`${import.meta.env.VITE_API_URL}/api/login`, {
+            const res = await fetch(`${import.meta.env.VITE_API_URL}/login`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
